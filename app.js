70836ac83d63f6964a318bbef0b6b392332562a0
Kristófer Gudmundsson <kristofergudmundsson@Kristofers-MacBook-Pro.local>
2018-10-10 16:15:50 +0000

    API, app, Circle CI

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 // app.js
 
-const greeting = require('./greeting');
-console.log(greeting('Totoro'));
+const app = require("./src/api");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
---
+++ src/api.js
@@ -0,0 +1,9 @@
+// src/api.js
+const express = require("express");
+const app = express();
+const greeting = require("./greeting");
+app.get("/greeting/:name", (req, res) => {
+  res.status(200);
+  res.send({greeting : "Hello, " + req.params.name + "!"});
+});
+module.exports = app;
